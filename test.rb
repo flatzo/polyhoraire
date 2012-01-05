c446e4b8c16c8e4eb80d44ff11829825e838dbb9
Charles Briere <charlesbriere.flatzo@gmail.com>
2012-01-05 15:11:36 -0500

    wtf

--- test.rb
+++ test.rb
@@ -1,36 +1,14 @@
 #!/usr/bin/ruby
 
-require 'rubygems'
-require 'google/api_client'
-require 'yaml'
+require 'nokogiri'
 
-oauth_yaml = YAML.load_file('.google-api.yaml')
-client = Google::APIClient.new
-client.authorization.client_id = oauth_yaml["client_id"]
-client.authorization.client_secret = oauth_yaml["client_secret"]
-client.authorization.scope = oauth_yaml["scope"]
-client.authorization.refresh_token = oauth_yaml["refresh_token"]
-client.authorization.access_token = oauth_yaml["access_token"]
+xml = Nokogiri::XML(File.read('test/asset/xmlSchedule.xml'))
+params = Hash.new
+params['debutB1'] = '2012-01-09'
+params['debutB2'] = '2012-01-15'
+params['fin'] = '20120309'
+xsl = Nokogiri::XSLT(File.read('src/google.xml.xsl'))
 
-if client.authorization.refresh_token && client.authorization.expired?
-  client.authorization.fetch_access_token!
-end
+cal = xsl.transform(xml,params)
 
-service = client.discovered_api('calendar', 'v3')
-
-
-
-page_token = nil
-result = client.execute(:api_method => service.calendar_list.list)
-while true
-  entries = result.data.items
-  entries.each do |e|
-    print e.summary + "\n"
-    print e.id + "\n"
-  end
-  if !(page_token = result.data.next_page_token)
-    break
-  end
-  result = client.execute(:api_method => service.calendar_list.list,
-                          :parameters => {'pageToken' => page_token})
-end
+File.open('test/asset/cal.xml','w') {|f| cal.write_xml_to f}
